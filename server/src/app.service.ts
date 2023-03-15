a8297cb106fb12acdf82c11677477fa8060648bf
neilchang01 <nchang@palo-it.com>
2023-03-15 15:50:37 +0800

    Added service layer function with temp return value

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  shorten(url: string): string {
+    const hash = Math.random().toString(36).slice(7); //temp
+    return hash;
   }
 }
