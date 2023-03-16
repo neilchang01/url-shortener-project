ec08dd625ea689ec545eabcd960e790ae7e4aa1a
neilchang01 <nchang@palo-it.com>
2023-03-16 21:30:37 +0800

    Added allowed origins

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: ['http://localhost:5173', 'https://url-shortener-nc.netlify.app'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
